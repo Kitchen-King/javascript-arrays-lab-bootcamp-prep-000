daf1d7b042e60e73b26bbeb3a3cf7255bce2585e
Kyle Spencer <spencerk2@vcu.edu>
2018-05-25 22:16:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(a) {kittens.push(a); return kittens}
+
+function destructivelyPrependKitten(a){kittens.unshift(a); return kittens}
+
+function destructivelyRemoveLastKitten(a){kittens.pop(a); return kittens}
+
+function destructivelyRemoveFirstKitten(a){kittens.shift(a); return kittens}
+
+function appendKitten(a) {
