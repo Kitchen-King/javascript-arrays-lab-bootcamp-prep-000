0cb4a9112e6b2eed88ed07128d3e1fcf672239ec
Kyle Spencer <spencerk2@vcu.edu>
2018-05-25 22:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ function prependKitten(a) {var NewKittens = [a,...kittens]; return NewKittens}
 
 function removeFirstKitten() {var NewKittens = kittens.slice(1); return NewKittens}
 
-function removeLasttKitten() {var NewKittens = kittens.slice(0, kittens.length - 1); return NewKittens}
+function removeLastKitten() {var NewKittens = kittens.slice(0, kittens.length - 1); return NewKittens}
